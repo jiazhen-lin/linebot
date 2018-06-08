a25c06849f1e97b26ccc8017578af51371c0250f
Chih Kai Yu <kai.chihkaiyu@gmail.com>
2018-06-09 00:12:34 +0800

    :recycle: Add config instance for every package

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"github.com/jiazhen-lin/linebot/api"
 	"github.com/jiazhen-lin/linebot/command"
+	"github.com/jiazhen-lin/linebot/config"
 	"github.com/jiazhen-lin/linebot/server"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	log := logrus.New()
-	srv := server.New(log)
-	cmd := command.New(log)
+	config, err := config.New()
+	if err != nil {
+		log.Errorf("%s", err)
+	}
+	srv := server.New(log, config)
+	cmd := command.New(log, config)
 
 	api.NewBotAPIs(srv, cmd)
-	api.NewIndexAPIs(srv)
+	api.NewIndexAPIs(srv, log)
 
 	srv.Run()
 }
