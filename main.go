38d27276643b844366756e757face714ce07d2ef
Chih Kai Yu <kai.chihkaiyu@gmail.com>
2018-07-07 22:37:13 +0800

    :sparkles: Add linebot command handler interface

--- main.go
+++ main.go
@@ -12,12 +12,24 @@ func main() {
 	log := logrus.New()
 	config, err := config.New()
 	if err != nil {
-		log.Errorf("%s", err)
+		log.Error(err)
+	}
+	bot, err := linebot.New(config.LineConfig.Secret, config.LineConfig.Token)
+	if err != nil {
+		log.Error(err)
 	}
 	srv := server.New(log, config)
 	cmd := command.New(log, config)
 
-	api.NewBotAPIs(srv, cmd)
+	// Linebot command handler
+	follow := command.NewFollowCommand()
+	unFollow := command.NewUnfollowCommand()
+	join := command.NewJoinCommand()
+	leave := command.NewLeaveCommand()
+	postback := command.NewPostbackCommand()
+	message := command.NewMessageCommand()
+
+	api.NewBotAPIs(srv, bot, follow, unFollow, join, leave, postback, message)
 	api.NewIndexAPIs(srv, log)
 
 	srv.Run()
