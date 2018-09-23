aca842015331c8ee1024819b36958e106b259efe
Chih Kai Yu <kai.chihkaiyu@gmail.com>
2018-09-24 01:06:32 +0800

    :sparkles: Add cost command parser

--- command/message.go
+++ command/message.go
@@ -3,12 +3,15 @@ package command
 import (
 	"context"
 	"strings"
+	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/line/line-bot-sdk-go/linebot"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 type messageCMD struct {
+	db *sqlx.DB
 }
 
 type costParam struct {
@@ -24,8 +27,8 @@ func parseCostCommand(cmd []string) (*costParam, error) {
 	a := kingpin.New("cost", "Accounting command line.")
 	price := a.Arg("price", "Price").Required().Float64()
 	category := a.Arg("category", "Category").Required().String()
-	accountingTime := a.Flag("time", "Accounting time.").Short('t').String()
-	detail := a.Flag("detail", "Detail.").Short('d').String()
+	accountingTime := a.Flag("time", "Accounting time.").Default(time.Now().UTC().Format("2006-01-02")).Short('t').String()
+	detail := a.Flag("detail", "Detail.").Default("").Short('d').String()
 	_, err := a.Parse(cmd)
 	if err != nil {
 		return nil, err
@@ -39,15 +42,26 @@ func (c *messageCMD) Command(ctx context.Context, event *linebot.Event) ([]lineb
 	case *linebot.TextMessage:
 		splitedCommand := strings.Split(message.Text, " ")
 		if splitedCommand[0] == "cost" || splitedCommand[0] == "Cost" {
+			// parse cost command
 			param, err := parseCostCommand(splitedCommand[1:])
 			if err != nil {
 				m := linebot.NewTextMessage(err.Error())
 				return []linebot.Message{m}, nil
 			}
-		} else {
-			m := linebot.NewTextMessage("Unknown command!\n" + costUsage)
+			userID := event.Source.UserID
+
+			// insert new record to db
+			insert := "INSERT INTO Accounting (userID, category, accountingTime, price, detail) VALUES (?, ?, ?, ?)"
+			_, err = c.db.Exec(insert, userID, param.category, param.accountingTime, param.price, param.detail)
+			if err != nil {
+				m := linebot.NewTextMessage(err.Error())
+				return []linebot.Message{m}, nil
+			}
+			m := linebot.NewTextMessage("Accounting recorded!")
 			return []linebot.Message{m}, nil
 		}
+		m := linebot.NewTextMessage("Unknown command!\n" + costUsage)
+		return []linebot.Message{m}, nil
 	default:
 		m := linebot.NewTextMessage("Not supported event yet, sorry!")
 		return []linebot.Message{m}, nil
@@ -59,6 +73,6 @@ func (c *messageCMD) Register(cmd string, handler func() error) error {
 }
 
 // NewMessageCommand returns command concrete instance
-func NewMessageCommand() Interface {
-	return &messageCMD{}
+func NewMessageCommand(db *sqlx.DB) Interface {
+	return &messageCMD{db}
 }
